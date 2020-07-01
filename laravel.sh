ff0c8c89a1641e2b63fd6b863b0e630dec58084c
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2020-07-01 15:26:03 +0300

    Update laravel.sh

--- laravel.sh
+++ laravel.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update
-sudo apt install -y php7.2-common php7.2-cli php7.2-gd php7.2-mysql php7.2-pgsql php7.2-curl php7.2-intl php7.2-mbstring php7.2-bcmath php7.2-imap php7.2-xml php7.2-zip
+sudo apt install -y php7.4-common php7.4-cli php7.4-gd php7.4-mysql php7.4-pgsql php7.4-curl php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-imap php7.4-xml php7.4-zip
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
